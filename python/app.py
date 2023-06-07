297a89c6ccbc6bbb935cc59067c55737027a610f
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-06 20:37:54 -0600

    added dump

--- python/app.py
+++ python/app.py
@@ -1,6 +1,6 @@
 #importing
 import json
-from flask import Flask
+from flask import Flask, request
 import dbhelper
 
 app = Flask(__name__)
@@ -10,10 +10,25 @@ def select_dogs():
    #function that returns the results from run_proceedure when using respective arguments as JSON
    results = dbhelper.run_proceedure('CALL return_all', [])
    if(type(results)==list):
-    results_json = json.dumps(results, default=str)
+    return json.dumps(results, default=str)
    else:
-      print('error')
-   return results_json
+     return "something has gone wrong"
+ 
 
 #running @app
+
+
+
+@app.get('/api/loyal_clients')
+def loyal_clients():
+   #function that returns the results from run_proceedure when using respective arguments as JSON
+   max_points = request.args.get('max_points')
+   results = dbhelper.run_proceedure('CALL loyal_clients(?)', [max_points])
+   if(type(results)==list):
+    return json.dumps(results, default=str)
+   else:
+      return "something has gone wrong"
+
+
+
 app.run(debug=True)
